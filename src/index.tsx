2ada73ea0461de411ec47ddc280ae2e7f02fa7df
Richard Lindhout <richardlindhout96@gmail.com>
2021-11-19 11:54:36 +0100

    fix: export some types

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { default as useFormState } from './useFormState';
 export { default as Form } from './Form';
 export { default as ScrollView } from './ScrollView';
+export type { ErrorUtility, BooleanUtility } from './types';
