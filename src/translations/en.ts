8501ff846cb4f62f16594aef696be4e13e26e4d6
Richard Lindhout <richardlindhout96@gmail.com>
2022-12-15 12:01:32 +0100

    feat: add support for locale + regex + simpler code

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -1,17 +1,17 @@
 import type { TranslationsType } from './utils';
 
 const en: TranslationsType = {
-  required: (params) => `${params.fieldKey || params.fieldKey} is required`,
+  required: (params) => `${params.label || params.fieldKey} is required`,
   lengtShouldBeLongerThan: (params) =>
-    `${params.fieldKey || params.fieldKey} length should be longer than ${
+    `${params.label || params.fieldKey} length should be longer than ${
       params.requiredLength
     }`,
   lengthShouldBeShorterThan: (params) =>
-    `${params.fieldKey || params.fieldKey} length should be shorter than ${
+    `${params.label || params.fieldKey} length should be shorter than ${
       params.requiredLength
     }`,
   shouldFollowRegex: (params) =>
     params.errorMessage ||
-    `${params.fieldKey || params.fieldKey} is not in the right format`,
+    `${params.label || params.fieldKey} is not in the right format`,
 };
 export default en;
--- src/translations/nl.ts
+++ src/translations/nl.ts
@@ -1,17 +1,17 @@
 import type { TranslationsType } from './utils';
 
 const nl: TranslationsType = {
-  required: (params) => `${params.fieldKey || params.fieldKey} is required`,
+  required: (params) => `${params.label || params.fieldKey} is required`,
   lengtShouldBeLongerThan: (params) =>
-    `${params.fieldKey || params.fieldKey} moet langer zijn dan ${
+    `${params.label || params.fieldKey} moet langer zijn dan ${
       params.requiredLength
     }`,
   lengthShouldBeShorterThan: (params) =>
-    `${params.fieldKey || params.fieldKey} moet korter zijn dan ${
+    `${params.label || params.fieldKey} moet korter zijn dan ${
       params.requiredLength
     }`,
   shouldFollowRegex: (params) =>
     params.errorMessage ||
-    `${params.fieldKey || params.fieldKey} is niet in het juiste formaat`,
+    `${params.label || params.fieldKey} is niet in het juiste formaat`,
 };
 export default nl;
